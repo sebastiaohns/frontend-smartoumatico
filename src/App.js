f14e114d92a31d4f59a4daec893adbb4b6609a64
sebastiaohns <sebastiaohns@gmail.com>
2019-09-29 22:38:47 -0300

    Deleted unused files added routes and button

--- src/App.js
+++ src/App.js
@@ -1,59 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import Countdown from 'react-countdown-now';
-import logo from './logo.svg';
 import './App.css';
+
+import Routes from './routes'
 
 function App() {
 
-  const date = '2019-10-29T01:02:03';
-
   return (
-    <div className="App">
-      <div className="Container">
-        <p className="TitleCountDown">O próximo relatório será enviado automaticamente em </p>
-         <div className="CountDown">
-          <div className="Day">
-            <Countdown
-              date={date}
-              renderer={props => <span>{props.days}</span>}
-              zeroPadTime={2}
-            />
-            <p>Dias</p>
-          </div>
-          <div className="Hour">
-            <Countdown
-              date={date}
-              renderer={props => <span>{props.hours}</span>}
-              zeroPadTime={2}
-            />
-            <p>Horas</p>
-          </div>
-          <div className="Minute">
-            <Countdown
-              date={date}
-              renderer={props => <span>{props.minutes}</span>}
-              zeroPadTime={2}
-            />
-            <p>Mins</p>
-          </div>
-          <div className="Second">
-            <Countdown
-              date={date}
-              renderer={props => <span>{props.seconds}</span>}
-              zeroPadTime={2}
-            />
-            <p>Segs</p>
-          </div>
-        </div>
-      </div>
-
-      
-
-
-
-
-    </div>
+    <Routes/>
   );
 }
 
