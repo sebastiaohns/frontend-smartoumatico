f31b60a4026611b9245d42ac5f2364451a5c838f
sebastiaohns <sebastiaohns@gmail.com>
2019-10-15 12:51:48 -0300

    finished report and countdown style

--- src/components/Countdown.js
+++ src/components/Countdown.js
@@ -7,41 +7,27 @@ export default function CountdownSection() {
   const date = '2019-10-29T01:02:03';
 
   return (
-    <section className="Section">
-      <p className="TitleCountDown">O próximo relatório será enviado automaticamente em </p>
-      <div className="Container CountDown">
-        <div className="Day">
-          <Countdown
-            date={date}
-            renderer={props => <span>{props.days}</span>}
-            zeroPadTime={2}
-          />
-          <p>Dias</p>
-        </div>
-        <div className="Hour">
-          <Countdown
-            date={date}
-            renderer={props => <span>{props.hours}</span>}
-            zeroPadTime={2}
-          />
-          <p>Horas</p>
-        </div>
-        <div className="Minute">
-          <Countdown
-            date={date}
-            renderer={props => <span>{props.minutes}</span>}
-            zeroPadTime={2}
-          />
-          <p>Mins</p>
-        </div>
-        <div className="Second">
-          <Countdown
-            date={date}
-            renderer={props => <span>{props.seconds}</span>}
-            zeroPadTime={2}
-          />
-          <p>Segs</p>
-        </div>
+    <section className="Countdown">
+      <p className="Title">O próximo relatório será enviado automaticamente em </p>
+      <div className="Day">
+        <Countdown date={date}
+          renderer={props => <span>{props.days}</span>}/>
+        <p>Dias</p>
+      </div>
+      <div className="Hour">
+        <Countdown date={date}
+          renderer={props => <span>{props.hours}</span>}/>
+        <p>Horas</p>
+      </div>
+      <div className="Minute">
+        <Countdown date={date}
+          renderer={props => <span>{props.minutes}</span>}/>
+        <p>Mins</p>
+      </div>
+      <div className="Second">
+        <Countdown date={date}
+          renderer={props => <span>{props.seconds}</span>}/>
+        <p>Segs</p>
       </div>
     </section>
   );
--- src/components/Report.js
+++ src/components/Report.js
@@ -1,44 +1,60 @@
 import React from 'react';
 import './Report.css';
 
-import { ReactComponent as User } from '../assets/user.svg';
-import { ReactComponent as Flag } from '../assets/flag.svg';
-import { ReactComponent as Bubble } from '../assets/speech-bubble.svg';
-import { ReactComponent as Mortarboard } from '../assets/mortarboard.svg';
-import { ReactComponent as Book } from '../assets/open-book.svg';
+import { Award, BookOpen, Flag, MessageCircle, Users } from 'react-feather';
 
 export default function Report() {
 
   return (
-    <section className="Report">
-      <p>Quantidade de atividades enviadas para o SMART no mês de agosto</p>
-      <ul>
-        <li>
-          <User className="Icon"/>
-          <p className="Label">Profissionais Cadastrados</p>
-          <p className="Data">123</p>
-        </li>
-        <li>
-          <Flag className="Icon"/>
-          <p className="Label">Pontos Implantados</p>
-          <p className="Data">123</p>
-        </li>
-        <li>
-          <Bubble className="Icon"/>
-          <p className="Label">Teleconsultorias Produzidas</p>
-          <p className="Data">123</p>
-        </li>
-        <li>
-          <Mortarboard className="Icon"/>
-          <p className="Label">Atividades de Tele-educação Produzidas</p>
-          <p className="Data">11223</p>
-        </li>
-        <li>
-          <Book className="Icon"/>
-          <p className="Label">Objetos de Aprendizagem Produzidos</p>
-          <p className="Data">123</p>
-        </li>
-      </ul>
+    <section>
+      <div className="Report">
+        <p className="Title">Quantidade de atividades enviadas para o SMART no mês de agosto</p>
+        <ul>
+          <li>
+            <Users className="Icon"/>
+            <p className="Label">Profissionais Cadastrados</p>
+            <p>123</p>
+          </li>
+          <li>
+            <Flag className="Icon"/>
+            <p className="Label">Pontos Implantados</p>
+            <p>123</p>
+          </li>
+          <li>
+            <MessageCircle className="Icon"/>
+            <p className="Label">Teleconsultorias Produzidas</p>
+            <p>123</p>
+          </li>
+          <li>
+            <Award className="Icon"/>
+            <p className="Label">Atividades de Tele-educação Produzidas</p>
+            <p>11223</p>
+          </li>
+          <li>
+            <BookOpen className="Icon"/>
+            <p className="Label">Objetos de Aprendizagem Produzidos</p>
+            <p>123</p>
+          </li>
+        </ul>
+      </div>
+      <div className="Menu">
+        <select name="Mês">
+          <option value="Janeiro">Janeiro</option>
+          <option value="Fevereiro">Fevereiro</option>
+          <option value="Março">Março</option>
+          <option value="Abril">Abril</option>
+          <option value="Maio">Maio</option>
+          <option value="Junho">Junho</option>
+          <option value="Julho">Julho</option>
+          <option value="Agosto">Agosto</option>
+          <option value="Setembro">Setembro</option>
+          <option value="Outubro">Outubro</option>
+          <option value="Novembro">Novembro</option>
+          <option value="Dezembro">Dezembro</option>
+        </select>
+      </div>
+
+      <button className="Button" type="submit">Baixar Relatório</button>
     </section>
   );
 }
